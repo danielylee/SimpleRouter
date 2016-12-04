f537b89ed11bdb4b7aa836d4ef1d661da3e03aef
danielylee <daniel.y.lee9@gmail.com>
2016-12-04 11:32:21 -0800

    added hosts and switches

--- final.py
+++ final.py
@@ -9,20 +9,27 @@ from mininet.node import RemoteController
 
 class final_topo(Topo):
   def build(self):
-    
+
     # Examples!
     # Create a host with a default route of the ethernet interface. You'll need to set the
-    # default gateway like this for every host you make on this assignment to make sure all 
+    # default gateway like this for every host you make on this assignment to make sure all
     # packets are sent out that port. Make sure to change the h# in the defaultRoute area
     # and the MAC address when you add more hosts!
     h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='1.1.1.1/24', defaultRoute="h1-eth0")
     h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='2.2.2.2/24', defaultRoute="h2-eth0")
+    h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='3.3.3.3/24', defaultRoute="h3-eth0")
+    h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='4.4.4.4/24', defaultRoute="h3-eth0")
+    h4 = self.addHost('h5',mac='00:00:00:00:00:05',ip='5.5.5.5/24', defaultRoute="h3-eth0")
+
 
     # Create a switch. No changes here from Lab 1.
     s1 = self.addSwitch('s1')
+    s2 = self.addSwitch('s2')
+    s3 = self.addSwitch('s3')
+    s4 = self.addSwitch('s4')
 
-    # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on 
-    # Host 2. This is representing the physical port on the switch or host that you are 
+    # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on
+    # Host 2. This is representing the physical port on the switch or host that you are
     # connecting to.
     self.addLink(s1,h1, port1=8, port2=0)
     self.addLink(s1,h2, port1=9, port2=0)
@@ -33,7 +40,7 @@ def configure():
   net.start()
 
   CLI(net)
-  
+
   net.stop()
 
 
