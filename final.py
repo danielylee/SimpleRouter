8461a636f7eff7ee31e4147b7262ef1327315cea
danielylee <daniel.y.lee9@gmail.com>
2016-12-07 20:30:33 -0800

    final additions

--- final.py
+++ final.py
@@ -15,24 +15,43 @@ class final_topo(Topo):
     # default gateway like this for every host you make on this assignment to make sure all
     # packets are sent out that port. Make sure to change the h# in the defaultRoute area
     # and the MAC address when you add more hosts!
-    h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='1.1.1.1/24', defaultRoute="h1-eth0")
-    h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='2.2.2.2/24', defaultRoute="h2-eth0")
-    h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='3.3.3.3/24', defaultRoute="h3-eth0")
-    h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='4.4.4.4/24', defaultRoute="h3-eth0")
-    h4 = self.addHost('h5',mac='00:00:00:00:00:05',ip='5.5.5.5/24', defaultRoute="h3-eth0")
+    h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='10.0.1.10/24', defaultRoute="h1-eth0")
+    h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='10.0.2.20/24', defaultRoute="h2-eth0")
+    h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='10.0.3.30/24', defaultRoute="h3-eth0")
+    h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='172.16.10.100/24', defaultRoute="h4-eth0")
+    h5 = self.addHost('h5',mac='00:00:00:00:00:05',ip='10.0.4.10/24', defaultRoute="h5-eth0")
 
 
     # Create a switch. No changes here from Lab 1.
-    s1 = self.addSwitch('s1')
-    s2 = self.addSwitch('s2')
-    s3 = self.addSwitch('s3')
-    s4 = self.addSwitch('s4')
+    s1 = self.addSwitch('s1') # Floor 1
+    s2 = self.addSwitch('s2') # Floor 2
+    s3 = self.addSwitch('s3') # Floor 3
+    s4 = self.addSwitch('s4') # Core Switch
+    s5 = self.addSwitch('s5') # Data Center
 
     # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on
     # Host 2. This is representing the physical port on the switch or host that you are
     # connecting to.
-    self.addLink(s1,h1, port1=8, port2=0)
-    self.addLink(s1,h2, port1=9, port2=0)
+
+    # Host 10 to Floor 1
+    self.addLink(s1,h1, port1=1, port2=0)
+    # Host 20 to Floor 2
+    self.addLink(s1,h2, port1=1, port2=0)
+    # Host 30 to Floor 3
+    self.addLink(s1,h3, port1=1, port2=0)
+    # Floor 1 connect to Core Switch
+    self.addLink(s3,h4, port1=2, port2=3)
+    # Floor 2 connect to Core Switch
+    self.addLink(s4,h5, port1=2, port2=4)
+    # Floor 3 connect to Core Switch
+    self.addLink(s3,s4, port1=2, port2=5)
+    # Untrusted Host connect to Core Switch
+    self.addLink(s4,h4, port1=6, port2=0)
+    # Core Switch connect to Data Center
+    self.addLink(s4,h5, port1=7, port2=2)
+    # Server connect to Data Center
+    self.addLink(s5,h5, port1=1, port2=0)
+
 
 def configure():
   topo = final_topo()
--- finalcontroller.py
+++ finalcontroller.py
@@ -1,9 +1,9 @@
 # Final Skeleton
 #
 # Hints/Reminders from Lab 4:
-# 
+#
 # To send an OpenFlow Message telling a switch to send packets out a
-# port, do the following, replacing <PORT> with the port number the 
+# port, do the following, replacing <PORT> with the port number the
 # switch should send the packets out:
 #
 #    msg = of.ofp_flow_mod()
@@ -37,12 +37,120 @@ class Final (object):
     connection.addListeners(self)
 
   def do_final (self, packet, packet_in, port_on_switch, switch_id):
-    # This is where you'll put your code. The following modifications have 
+    # This is where you'll put your code. The following modifications have
     # been made from Lab 4:
     #   - port_on_switch represents the port that the packet was received on.
     #   - switch_id represents the id of the switch that received the packet
     #      (for example, s1 would have switch_id == 1, s2 would have switch_id == 2, etc...)
-    print "Hello, World!"
+    if packet.find('ipv4') is None:
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.idle_timeout = 30
+        msg.hard_timeout = 30
+        msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+        msg.data = packet_in
+        self.connection.send(msg)
+    else:
+        if (switch_id == 4):
+            if (port_on_switch == 6 and packet.payload.find('icmp')):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.data = packet_in
+                self.connection.send(msg)
+        elif packet.find('ipv4') is not None and port_on_switch == 6:
+            if packet.find('ipv4').dstip == ('10.0.1.10'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 3))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.2.20'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 4))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.3.30'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 5))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.4.10'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.data = packet_in
+                self.connection.send(msg)
+        else:
+            if packet.find('ipv4').dstip == ('10.0.1.10'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 3))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.2.20'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 4))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.3.30'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 5))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('10.0.4.10'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 7))
+                msg.data = packet_in
+                self.connection.send(msg)
+            elif packet.find('ipv4').dstip == ('172.16.10.100'):
+                msg = of.ofp_flow_mod()
+                msg.match = of.ofp_match.from_packet(packet)
+                msg.idle_timeout = 30
+                msg.hard_timeout = 30
+                msg.actions.append(of.ofp_action_output(port = 6))
+                msg.data = packet_in
+                self.connection.send(msg)
+        else:
+        # check for IP traffic
+            if packet.find('ipv4') is not None:
+                if port_on_switch == 1:
+                    msg = of.ofp_flow_mod()
+                    msg.match = of.ofp_match.from_packet(packet)
+                    msg.idle_timeout = 30
+                    msg.hard_timeout = 30
+                    msg.actions.append(of.ofp_action_output(port = 2))
+                    msg.data = packet_in
+                    self.connection.send(msg)
+                else:
+                    msg = of.ofp_flow_mod()
+                    msg.match = of.ofp_match.from_packet(packet)
+                    msg.idle_timeout = 30
+                    msg.hard_timeout = 30
+                    msg.actions.append(of.ofp_action_output(port = 1))
+                    msg.data = packet_in
+                    self.connection.send(msg)
 
   def _handle_PacketIn (self, event):
     """
